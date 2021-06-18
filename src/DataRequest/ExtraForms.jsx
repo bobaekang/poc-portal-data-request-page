a4c2f11e152f5a9fd396b17b4b5ba17714f1f7e0
Bobae Kang <bkang.dev@gmail.com>
2021-06-18 12:03:24 -0500

    Create an extra form for cohort details

--- src/DataRequest/ExtraForms.jsx
+++ src/DataRequest/ExtraForms.jsx
@@ -125,3 +125,67 @@ CohortExtraForm.propTypes = {
   onAddCohort: PropTypes.func,
   onClose: PropTypes.func,
 };
+
+/**
+ * @param {object} prop
+ * @param {ExplorerCohort[]} prop.cohorts
+ * @param {() => void} prop.onClose
+ */
+export function CohortDetailsForm({ cohorts, onClose }) {
+  const options = cohorts.map((cohort) => ({
+    label: cohort.name,
+    value: cohort,
+  }));
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <ExtraFormLayout title="Details on a selected Cohort" onClose={onClose}>
+      <form onKeyDown={(e) => e.preventDefault()}>
+        <SimpleInputField
+          label="Name"
+          input={
+            <Select
+              options={options}
+              value={selected}
+              autoFocus
+              clearable={false}
+              onChange={(e) => setSelected(e)}
+            />
+          }
+        />
+        <SimpleInputField
+          label="Description"
+          input={
+            <textarea
+              disabled
+              placeholder="No description"
+              value={selected.value.description}
+            />
+          }
+        />
+        <SimpleInputField
+          label="Filters"
+          input={
+            <textarea
+              disabled
+              placeholder="No filters"
+              value={stringifyFilters(selected.value.filters)}
+            />
+          }
+        />
+      </form>
+    </ExtraFormLayout>
+  );
+}
+
+CohortDetailsForm.propTypes = {
+  cohorts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+      filters: PropTypes.arrayOf(PropTypes.object),
+      id: PropTypes.number,
+    }),
+  ),
+  onClose: PropTypes.func,
+};
